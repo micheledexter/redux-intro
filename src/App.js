e5a840ebaba8510e9d0b4ff0294096b6f45160d4
Shely Dexter <micheleannedexter@gmail.com>
2018-05-22 12:10:47 -0500

    basic input box to set reducer states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      midplace: {
+        first: '',
+        second: '',
+      }
+    }
+  }
+  buttonOneClicked = () => {
+    const action = { type: 'BUTTON_ONE', someProp: 'Hello!' };
+    this.props.dispatch(action);
+  };
+
+  handleElementChange = propertyName => event => {
+    this.setState({
+      midplace: {
+        ...this.state.midplace,
+        [propertyName]: event.target.value,
+      }
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        
+        <button onClick={this.buttonOneClicked}>Button One</button>
+        <button onClick={() => this.props.dispatch({ type: 'MINUS' })}>Minus</button><br />
+        <button onClick={() => this.props.dispatch({ type: 'BUTTON_TWO' })}>Button Two</button>
+        <button onClick={() => this.props.dispatch({ type: 'REMOVE_OREO' })}>Eat an Oreo</button><br />
+        <input onChange={this.handleElementChange('first')} />
+        <button onClick={() => this.props.dispatch({ type: 'SET_FIRST', state: this.state.midplace.first })}>Set first</button>
+        <input onChange={this.handleElementChange('second')} />
+        <button onClick={() => this.props.dispatch({ type: 'SET_SECOND', state: this.state.midplace.second })}>Set second</button>
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -3,14 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-const storeInstance = createStore(
-    () => {
-        console.log('A store has been created!');
+// The action is what we dispatched from App.js
+const firstReducer = (state = 0, action) => {
+    if (action.type === 'BUTTON_ONE') {
+        console.log('First reducer', action);
+        return ++state;
+    } else if  (action.type === 'MINUS') {
+        return --state;
+    } else if (action.type === 'SET_FIRST') {
+        return (state = action.state);
     }
-)
+    return state; // Return next state
+};
+
+const secondReducer = (state = [], action) => {
+    if (action.type === 'BUTTON_TWO') {
+        console.log('Second reducer', action);
+        return [...state, 'oreo']; // Add oreo to state
+    } else if (action.type === 'REMOVE_OREO') {
+        state.pop();
+        return state;
+    } else if (action.type === 'SET_SECOND') {
+        return (state = action.state);
+    }
+    return state;
+};
+
+// Create store can only take in one reducer
+const storeInstance = createStore(
+    // Combine the reducers and do some extra validation
+    combineReducers({
+        firstReducer, // same as `firstReducer: firstReducer`
+        secondReducer,
+    }),
+    applyMiddleware(logger),
+);
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
