a49bf9e11235de23db3f8185cd4decc78cb94893
Shely Dexter <micheleannedexter@gmail.com>
2018-05-22 10:11:26 -0500

    implement redux and react-redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const storeInstance = createStore(
+    () => {
+        console.log('A store has been created!');
+    }
+)
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
